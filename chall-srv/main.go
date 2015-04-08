c00b9945d0d941c7ba18285d56c83ca751a9384e
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-04-07 17:48:12 -0700

    fully functional challenge (simpleHTTPS and DVSNI) server... if things are in redis that is :>

--- chall-srv/main.go
+++ chall-srv/main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/pem"
 	"fmt"
 	"net"
 	"net/http"
 	"crypto/tls"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"math/big"
 	"strings"
 	"time"
+
+	"github.com/letsencrypt/boulder/core"
+	"github.com/garyburd/redigo/redis"
 )
 
-func genSelfSigned(dnsNames []string) (cert *tls.Certificate) {
+func newPool(server, password string) *redis.Pool {
+    return &redis.Pool{
+        MaxIdle: 3,
+        IdleTimeout: 240 * time.Second,
+        Dial: func () (redis.Conn, error) {
+            c, err := redis.Dial("tcp", server)
+            if err != nil {
+                return nil, err
+            }
+            if password != "" {
+	            if _, err := c.Do("AUTH", password); err != nil {
+	                c.Close()
+	                return nil, err
+	            }
+            }
+            return c, err
+        },
+        TestOnBorrow: func(c redis.Conn, t time.Time) error {
+            _, err := c.Do("PING")
+            return err
+        },
+    }
+}
+
+var (
+    pool *redis.Pool
+)
+
+func genSelfSigned(dnsNames []string, pubKey *rsa.PublicKey) (cert *tls.Certificate) {
 	template := &x509.Certificate{
 		SerialNumber: big.NewInt(1337),
 		Subject: pkix.Name{
@@ -35,9 +69,8 @@ func genSelfSigned(dnsNames []string) (cert *tls.Certificate) {
 		fmt.Println("couldn't generate rsa 2048 key", err)
 		return &tls.Certificate{}
 	}
-	pub := &priv.PublicKey
 
-	certBytes, err := x509.CreateCertificate(rand.Reader, template, template, pub, priv)
+	certBytes, err := x509.CreateCertificate(rand.Reader, template, template, pubKey, priv)
 	if err != nil {
 		fmt.Println("couldn't create cert", err)
 		return &tls.Certificate{}
@@ -50,25 +83,97 @@ func genSelfSigned(dnsNames []string) (cert *tls.Certificate) {
 	return
 }
 
-// creates the right certificate with relevant DNS names based on provided ServerName
+func getPubKey(dnsName string, rConn redis.Conn) (pubKey *rsa.PublicKey, err error) {
+	rKey := fmt.Sprintf("pk:%s", dnsName)
+	var pubBytes []byte
+	pubBytes, err = redis.Bytes(rConn.Do("GET", rKey))
+	if err != nil {
+		return
+	}
+
+	var block *pem.Block
+	block, _ = pem.Decode(pubBytes)
+
+	var pub interface{}
+	pub, err = x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+
+	pubKey, ok := pub.(*rsa.PublicKey)
+	if !ok {
+		err = fmt.Errorf("Didn't get a RSA public key...")
+		return
+	}
+
+	return
+}
+
+// creates the right certificate with relevant DNS names and public key based on provided ServerName
 // (from SNI).
 func getCert(clientHello *tls.ClientHelloInfo) (cert *tls.Certificate, err error) {
-	dnsNames := []string{clientHello.ServerName}
-	if strings.HasSuffix(clientHello.ServerName, ".acme.invalid") {
-		// actually check what this is by getting secret from servername
-		domain := "example.com"
-		dnsNames = append(dnsNames, domain)
-	}
-	// should also get public key for the real domain (+with a else here! or something)
+	rConn := pool.Get()
+	defer rConn.Close()
 
-	cert = genSelfSigned(dnsNames)
+	var dnsNames []string
+	//var publicKey string
+	if strings.HasSuffix(clientHello.ServerName, ".acme.invalid") {
+		// DVSNI challenge so lets compute Z... FUN
+		nonce := clientHello.ServerName[0:len(clientHello.ServerName)-13]
+
+		var packed string
+		// this may conflict since the draft implicitly says nonces may be reused
+		// should CHECK the boulder src to see what it does...
+		packed, err = redis.String(rConn.Do("GET", nonce))
+		if err != nil {
+			return
+		}
+
+		//assume packed is in form 'dnsName:r:s:pubKey'
+		unpacked := strings.SplitN(packed, ":", 4)
+		if len(unpacked) < 4 {
+			err = fmt.Errorf("Value for key [%s] in incorrect format [%s]", nonce, packed)
+			return
+		}
+
+		var R, S []byte
+		R, err = core.B64dec(unpacked[1])
+		if err != nil {
+			return
+		}
+		S, err = core.B64dec(unpacked[2])
+		if err != nil {
+			return
+		}
+		RS := append(R, S...)
+		z := sha256.Sum256(RS)
+		zName := hex.EncodeToString(z[:])
+		zName = fmt.Sprintf("%s.acme.invalid", zName)
+
+		domain := unpacked[0]
+		if err != nil {
+			return
+		}
+		dnsNames = append(dnsNames, domain, zName)
+	} else {
+		dnsNames = []string{clientHello.ServerName}
+	}
+	
+	var pubKey *rsa.PublicKey
+	pubKey, err = getPubKey(dnsNames[0], rConn)
+	if err != nil {
+		return
+	}
+
+	cert = genSelfSigned(dnsNames, pubKey)
 	return
 }
 
 func listenAndMeepMeep(srv *http.Server) error {
+	nullPk := rsa.PublicKey{N:big.NewInt(1337) , E: 10}
 	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{*genSelfSigned([]string{"null"})}, // because servers require at least one...
-		ClientAuth: tls.NoClientCert,
+		Certificates: []tls.Certificate{*genSelfSigned([]string{"roadrunner"}, &nullPk)}, // because Listener requires at least one
+		ClientAuth: tls.NoClientCert,                                               // and it'll never actually be served
 		GetCertificate: getCert,
 		NextProtos: []string{"http/1.1"},
 	}
@@ -83,11 +188,25 @@ func listenAndMeepMeep(srv *http.Server) error {
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example server.\n"))
+	if !strings.HasSuffix(req.TLS.ServerName, ".acme.invalid") {
+		rConn := pool.Get()
+		defer rConn.Close()
+
+		key := fmt.Sprintf("ct:%s", req.TLS.ServerName)
+		token, err := redis.Bytes(rConn.Do("GET", key))
+		if err != nil {
+			fmt.Println("couldn't get challenge token", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write(token)
+	}
 }
 
 func main() {
+	pool = newPool(":6379", "")
+
 	http.HandleFunc("/", handler)
 
 	httpsServer := &http.Server{Addr: ":443"}
