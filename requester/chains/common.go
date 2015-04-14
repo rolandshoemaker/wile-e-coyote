7bd25eab8efe652265a93d41430dcfbeed7a3409
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-04-14 03:15:50 -0700

    requester main structure, wfe+test chains need to be written now

--- requester/chains/common.go
+++ requester/chains/common.go
@@ -1,8 +1,50 @@
 package chains
 
 import  (
+	"crypto/rsa"
+	"crypto/x509"
+	"fmt"
+	"time"
+
 	"github.com/garyburd/redigo/redis"
 )
+
+type requestResult struct {
+    Uri        string        `json:"uri,omitempty"`        // endpoint being hit e.g. "/acme/new-reg"
+    Took       time.Duration `json:"took,omitempty"`       // time request actually took
+    Successful bool          `json:"successful,omitempty"` // if the *right* thing happened (subjective...)
+    Error      string        `json:"error,omitempty"`      // if not right thing, what went wrong
+}
+
+type ChainResult struct {
+    ChainName         string          `json:"chainname,omitempty"`         // which chain is this result from
+    ChainSuccessful   bool            `json:"chainsuccessful,omitempty"`   // was the entire chain successful?
+    ChainTook         time.Duration   `json:"chaintook,omitempty"`         // how long did the entire chain take
+    IndividualResults []requestResult `json:"individualresults,omitempty"` // individual requestResults
+}
+
+type testChain struct {
+	testFunc     func() ChainResult
+	requirements []string
+}
+
+var TestChains []testChain = []testChain{testChain{testFunc: func() ChainResult {return ChainResult{}}, requirements: []string{"woop"}}}
+
+// public functions
+
+// return a *random* test chain from TestChains that
+// satifies testChain.requirements (based on whats in SQL)
+func GetChain() func() ChainResult {
+	return TestChains[0].testFunc
+}
+
+// internal stuff
+
+func satifiesRequirements() bool {
+	return true
+}
+
+// utility functions
 
 func setRedisPubKey(dnsName string, pubKey *rsa.PublicKey, rConn redis.Conn) error {
 	rKey := fmt.Sprintf("pk:%s", dnsName)
@@ -18,4 +60,13 @@ func setRedisPubKey(dnsName string, pubKey *rsa.PublicKey, rConn redis.Conn) err
 
 	return nil
 }
+
+func timedPOST(url string, data interface{}) {
+
+}
+
+func timedGET(url string) {
+
+}
+
 
--- requester/main.go
+++ requester/main.go
@@ -1,16 +1,73 @@
 package main
 
 import  (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/rolandshoemaker/wile-e-coyote/requester/chains"
 )
 
-func runChain(fn func) {
-	// run the chain and get the result
-	chainResult := fn()
+var numAttackers int = 10
+var results []chains.ChainResult
 
-	// write the result to some kind of log... (RPC calls to log server(?) that also displays the charts n stuff?)
+func attacker(closeChan chan bool) {
+	fmt.Println("starting attacker")
+	for {
+		select {
+		case <- closeChan:
+			// goodbye cruel world
+			break
+		default:
+			testChain := chains.GetChain()
+			chainResult := testChain()
+			results = append(results, chainResult)
+		}
+	}
+}
+
+func runAttacker() chan bool {
+	closeChan := make(chan bool, 1)
+	go attacker(closeChan)
+	return closeChan
+}
+
+func monitorHerd(alive []chan bool) []chan bool {
+	numAlive := len(alive)
+	fmt.Println("herding", numAlive, numAttackers)
+	if numAttackers != numAlive {
+		if numAttackers < numAlive {
+			// randomly kill some attackers when they finish doing their thing...
+			// idk why randomly...
+			rand.Seed(time.Now().Unix())
+			for i := 0; i < (numAlive-numAttackers); i++ {
+				randCloseChan := alive[rand.Intn(numAlive-1)]
+				randCloseChan <- true
+			}
+		} else {
+			// start some new attackers 
+			for i := 0; i < (numAttackers-numAlive); i++ {
+				alive = append(alive, runAttacker())
+			}
+		}
+	}
+
+	return alive
 }
 
 func main() {
+	var aliveAttackers []chan bool
 
+	fmt.Println("um hi")
+
+	//go func() {
+	//	fmt.Println("um goroutine")
+		for {
+			aliveAttackers = monitorHerd(aliveAttackers)
+			time.Sleep(time.Second * 5)
+		}
+	//}()
+
+	// livin on the edge!
+	
 }
